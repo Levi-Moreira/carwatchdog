f61327c86815fe0d1c5a78ea895655ed658eef6b
Vinicius Maciel <viniciusfre@gmail.com>
2016-01-29 13:27:19 -0300

    settings 2

--- app/src/main/java/com/example/levi/watchdog/MainActivity.java
+++ app/src/main/java/com/example/levi/watchdog/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.levi.watchdog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        
 
     }
 
@@ -32,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
+
         if (id == R.id.action_settings) {
-            return true;
+            /*Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);*/
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
