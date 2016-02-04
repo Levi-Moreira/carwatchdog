3b7eddd1eed3459845bbf42a0fdd36d15e04f7db
Vinicius Maciel <viniciusfre@gmail.com>
2016-02-04 12:19:17 -0300

    btooth

--- app/src/main/java/com/example/levi/watchdog/BluetoothActivity.java
+++ app/src/main/java/com/example/levi/watchdog/BluetoothActivity.java
@@ -1,7 +1,7 @@
 package com.example.levi.watchdog;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
+import  android.support.v4.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -12,33 +12,49 @@ import android.content.IntentFilter;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
-    private static  BluetoothAdapter BTAdapter;
+    private static BluetoothAdapter BTAdapter;
+
     private static ArrayList<DeviceItem> deviceItemList;
-    private static ArrayAdapter<DeviceItem> mAdapter;
-    private static ArrayList<DeviceItem> deviceSearched;
+    private static ArrayList<DeviceItem> deviceSearched = new ArrayList<>();
+
+    private static ArrayAdapter<DeviceItem> mPairedAdapter;
+    private static ArrayAdapter<DeviceItem> mDiscoveredAdapter;
+
+
     public static int REQUEST_BLUETOOTH = 1;
 
-    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+    private  final BroadcastReceiver bReciever = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // Create a new device item
-                DeviceItem newDevice = new DeviceItem(device.getName(), device.getAddress(), "false");
-                // Add it to our adapter
-                deviceSearched.add(newDevice);
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
+            {
+                Toast.makeText(getApplicationContext(), "Discovery Started", Toast.LENGTH_SHORT).show();
             }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+                 {
+                    Toast.makeText(getApplicationContext(), "Discovery Finished", Toast.LENGTH_SHORT).show();
+                 }
+            if (BluetoothDevice.ACTION_FOUND.equals(action))
+                      {
+                            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                            // Create a new device item
+                            DeviceItem newDevice = new DeviceItem(device.getName(), device.getAddress(), "false");
+                            // Add it to our adapter
+                            Toast.makeText(getApplicationContext(), "Discovery Found Decies", Toast.LENGTH_SHORT).show();
+                            deviceSearched.add(newDevice);
+                            mDiscoveredAdapter.notifyDataSetChanged();
+                       }
         }
     };
 
@@ -67,6 +83,11 @@ public class BluetoothActivity extends AppCompatActivity {
             startActivityForResult(enableBT, REQUEST_BLUETOOTH);
         }
 
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+
+
         deviceItemList = new ArrayList<DeviceItem>();
 
         Set<BluetoothDevice> pairedDevices = BTAdapter.getBondedDevices();
@@ -77,45 +98,55 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         }
 
-        mAdapter = new ArrayAdapter<DeviceItem>(this,R.layout.devices_list,deviceItemList);
+        mPairedAdapter = new ArrayAdapter<DeviceItem>(this,R.layout.devices_list,deviceItemList);
         ListView listView = (ListView) findViewById(R.id.devices_list);
-        listView.setAdapter(mAdapter);
+        listView.setAdapter(mPairedAdapter);
 
     }
 
     public void addDevice(View view)
     {
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        mAdapter.clear();
-        this.registerReceiver(bReciever, filter);
-        BTAdapter.startDiscovery();
-        android.support.v4.app.DialogFragment  newFragment = new DevicesFoundDialog();
-        newFragment.show(getSupportFragmentManager(), "searcheddevices");
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bReciever, filter);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(bReciever, filter);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bReciever, filter);
+        mPairedAdapter.clear();
 
 
-    }
-
-    public class DevicesFoundDialog extends android.support.v4.app.DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            deviceSearched = new ArrayList<DeviceItem>();
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle(R.string.bt_dialog_message)
-                    .setItems(deviceSearched.toArray(new CharSequence[deviceSearched.size()]), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // The 'which' argument contains the index position
-                            // of the selected item
-                        }
-                    });
-            return builder.create();
-        }
-
-        @Override
-        public void onDestroy() {
-            super.onDestroy();
-            getActivity().unregisterReceiver(bReciever);
+        //cancel any prior BT device discovery
+        if (BTAdapter.isDiscovering()){
             BTAdapter.cancelDiscovery();
         }
+        BTAdapter.startDiscovery();
+
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(BluetoothActivity.this);
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                unregisterReceiver(bReciever);
+                BTAdapter.cancelDiscovery();
+                Toast.makeText(getApplicationContext(), "Discovery Unregistered", Toast.LENGTH_SHORT).show();
+            }
+        });
+        LayoutInflater inflater = getLayoutInflater();
+        View convertView = (View) inflater.inflate(R.layout.discovered_devices, null);
+        alertDialog.setView(convertView);
+
+        alertDialog.setTitle(R.string.bt_dialog_message);
+        ListView devicesList = (ListView) convertView.findViewById(R.id.discovered_d);
+
+        mDiscoveredAdapter = new ArrayAdapter<DeviceItem>(this,R.layout.devices_list,deviceSearched);
+
+       /*DeviceItem di = new DeviceItem("Levi","Levi","levi");
+        mDiscoveredAdapter.add(di);*/
+        devicesList.setAdapter(mDiscoveredAdapter);
+
+        alertDialog.show();
+
     }
 
 }
